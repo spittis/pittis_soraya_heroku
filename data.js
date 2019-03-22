99c1be2786035353a131271e567d74f0977d216f
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-21 21:56:06 -0400

    added some images

--- data.js
+++ data.js
@@ -4,16 +4,23 @@ const data = {
 
     user1: {
         name: "Deluxe Pizza",
-        role: "developer",
-        bio: "some bio info goes here",
+        role: "Classic",
+        bio: "Pepperoni, fresh mushrooms, green peppers, bacon, sliced tomatoes, Spanish onions",
         bioPic: "deluxe.jpg"
     },
 
     user2: {
         name: "Mediterannen Pizza",
-        role: "designer",
-        bio: "some bio info goes here",
+        role: "Classic",
+        bio: "Sundried black olives, Spanish onions, feta cheese, sundried tomatoes",
         bioPic: "med.jpg"
+    },
+
+    user3: {
+        name: "Nonna's Favourite",
+        role: "White",
+        bio: "It’s simple, but flavourful: tender chunks of plum tomato mixed with parmigiano cheese and a generous sprinkling of basil.",
+        bioPic: "nonna.jpg"
     }
 };
 
