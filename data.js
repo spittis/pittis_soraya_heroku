20d672079d48c16a14e77c254a6e50abce61ba0d
Soraya <31996896+spittis@users.noreply.github.com>
2019-03-22 19:15:21 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -4,13 +4,15 @@ const data = {
         name: "Deluxe Pizza",
         role: "Classic",
         bio: "Pepperoni, fresh mushrooms, green peppers, bacon, sliced tomatoes, Spanish onions",
+        cost: "$16.99",
         bioPic: "deluxe.jpg",
     },
 
     user2: {
-        name: "Mediterannen Pizza",
+        name: "Mediterranean Pizza",
         role: "Classic",
         bio: "Sundried black olives, Spanish onions, feta cheese, sundried tomatoes",
+        cost: "$12.99",
         bioPic: "med.jpg"
     },
 
@@ -18,6 +20,7 @@ const data = {
         name: "Nonna's Favourite",
         role: "White",
         bio: "It’s simple, but flavourful: tender chunks of plum tomato mixed with parmigiano cheese and a generous sprinkling of basil.",
+        cost: "$13.99",
         bioPic: "nonna.jpg"
     }
 
