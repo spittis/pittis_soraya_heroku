443be18a45ff34d0fee402c7b829a689e64947a1
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-21 21:22:19 -0400

    added images

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = {
+    name: "Trevor",
+    role: "confuserator",
+
+    user1: {
+        name: "Deluxe Pizza",
+        role: "developer",
+        bio: "some bio info goes here",
+        bioPic: "deluxe.jpg"
+    },
+
+    user2: {
+        name: "Mediterannen Pizza",
+        role: "designer",
+        bio: "some bio info goes here",
+        bioPic: "med.jpg"
+    }
+};
+
+module.exports = data;
+
